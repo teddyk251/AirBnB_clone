9f0ce9d027a74e046032a705f115de56bd1a7213
Teddy <teddylnk1@gmail.com>
2022-08-09 20:54:34 +0300

    fix errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,57 +1,56 @@
 #!/usr/bin/python3
-"""
-Module file_storage serializes and
-deserializes JSON types
-"""
+"""File Storage"""
 
+from os.path import exists
 import json
-from models.base_model import BaseModel
-from models.user import User
-
 
 class FileStorage:
-    """
-    Custom class for file storage
-    """
+    """for FileStorage"""
 
     __file_path = "file.json"
-    __objects = {}
+    __objects = dict()
 
     def all(self):
-        """
-        Returns dictionary representation of all objects
-        """
+        """ dictionary __objects"""
         return self.__objects
 
-    def new(self, object):
-        """sets in __objects the object with the key
-        <object class name>.id
-
-        Args:
-            object(obj): object to write
-
-        """
-        self.__objects[object.__class__.__name__ + '.' + str(object)] = object
+    def new(self, obj):
+        """sets key """
+        self.__objects[type(obj).__name__ + "." + obj.id] = obj
 
     def save(self):
-        """
-        serializes __objects to the JSON file
-        (path: __file_path)
-        """
-        with open(self.__file_path, 'w+') as f:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()
-                       }, f)
+        """serialize to JSON file"""
+        temp = dict()
+        for keys in self.__objects.keys():
+            temp[keys] = self.__objects[keys].to_dict()
+        with open(self.__file_path, mode='w') as jsonfile:
+            json.dump(temp, jsonfile)
 
     def reload(self):
-        """
-        deserializes the JSON file to __objects, if the JSON
-        file exists, otherwise nothing happens)
-        """
-        try:
-            with open(self.__file_path, 'r') as f:
-                dict = json.loads(f.read())
-                for value in dict.values():
-                    cls = value["__class__"]
-                    self.new(eval(cls)(**value))
-        except Exception:
-            pass
+        """deserializes JSON file to __objects"""
+        from ..base_model import BaseModel
+        from ..amenity import Amenity
+        from ..place import Place
+        from ..state import State
+        from ..city import City
+        from ..user import User
+        from ..review import Review
+
+        if exists(self.__file_path):
+            with open(self.__file_path) as jsonfile:
+                reloaded_dict = json.load(jsonfile)
+            for keys in reloaded_dict.keys():
+                if reloaded_dict[keys]['__class__'] == "BaseModel":
+                    self.__objects[keys] = BaseModel(**reloaded_dict[keys])
+                elif reloaded_dict[keys]['__class__'] == "User":
+                    self.__objects[keys] = User(**reloaded_dict[keys])
+                elif reloaded_dict[keys]['__class__'] == "State":
+                    self.__objects[keys] = State(**reloaded_dict[keys])
+                elif reloaded_dict[keys]['__class__'] == "City":
+                    self.__objects[keys] = City(**reloaded_dict[keys])
+                elif reloaded_dict[keys]['__class__'] == "Amenity":
+                    self.__objects[keys] = Amenity(**reloaded_dict[keys])
+                elif reloaded_dict[keys]['__class__'] == "Place":
+                    self.__objects[keys] = Place(**reloaded_dict[keys])
+                elif reloaded_dict[keys]['__class__'] == "Review":
+                    self.__objects[keys] = Review(**reloaded_dict[keys])
